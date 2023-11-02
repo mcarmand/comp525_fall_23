559bdcc8d1cdb6b0ccd4fe4194b334c3565b1a0d
Mason Armand <masonarmand1@gmail.com>
2023-11-02 18:14:52 -0400

    test cases

--- labs/lab4/practice_tests.py
+++ labs/lab4/practice_tests.py
@@ -1,11 +1,11 @@
 """
 test_parse_season.py
 Mihaela
-Created March 20, 2019. Updated October 15, 2019
+Created March 20, 2019. Updated October 15, 2019, November 2, 2023
 """
 
 import unittest
-from .practice_dictionaries import Practice
+from practice_dictionaries import Practice
 
 
 class TestParseSeasons(unittest.TestCase):
@@ -30,6 +30,17 @@ class TestParseSeasons(unittest.TestCase):
         }
         actual_result = self.p.parse_seasons(input1)
         expected_result = 'springwarmsummerhotfalljustrightwintercold'
+        self.assertEqual(actual_result, expected_result)
+
+    def test_update_inventory(self):
+        input1 = {
+            'milk' : 50,
+            'bread' : 20,
+            'soda' : 5
+        }
+        input2 = 5
+        actual_result = self.p.update_inventory(input1, input2)
+        expected_result = {'milk' : 55, 'bread' : 25, 'soda' : 10}
         self.assertEqual(actual_result, expected_result)
 
 
